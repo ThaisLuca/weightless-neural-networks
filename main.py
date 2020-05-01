b629a7da9420b7fc9ec9cf0bbd9eeb2bdac6fb1d
thaisluca <thaisluca@uol.com.br>
2020-05-01 18:19:53 -0300

    Missing tests and validation confusion_matrices

--- main.py
+++ main.py
@@ -9,13 +9,13 @@
 import wisardpkg as wp
 import pandas as pd
 import numpy as np
-import cv2
 import sys
 
 from pandas import compat
 compat.PY3 = True
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.metrics import confusion_matrix
 
 def load_dataset():
 	dataset = pd.read_csv('dataset/mnist_train.csv')
@@ -32,32 +32,42 @@ def preprocess(df):
 
 def main():
 
-	dataset = load_dataset()
-
-	train, test = train_test_split(dataset, test_size=0.3)
-	x_train = preprocess(train.drop(['label'], axis=1)).values.tolist()
-	y_train = train['label'].values.tolist()
-	x_test = preprocess(test.drop(['label'], axis=1)).values.tolist()
-	y_test = test['label'].values.tolist()
+	accuracy_train_scores = []
 
 	addressSize = 3     # number of addressing bits in the ram
 	ignoreZero  = False # optional; causes the rams to ignore the address 0
 
 	# False by default for performance reasons,
 	# when True, WiSARD prints the progress of train() and classify()
-	verbose = True
+	verbose = False
 
+	dataset = load_dataset()
+
+	train, test = train_test_split(dataset, test_size=0.3)
+	X = preprocess(train.drop(['label'], axis=1)).values.tolist()
+	Y = train['label'].values.tolist()
+	X_test = preprocess(test.drop(['label'], axis=1)).values.tolist()
+	Y_test = test['label'].values.tolist()
+
+	# Define model
 	wsd = wp.Wisard(addressSize, ignoreZero=ignoreZero, verbose=verbose)
 
-	# train using the input data
-	wsd.train(x_train,y_train)
+	kf = KFold(n_splits=2)
+	for train_index, val_index in kf.split(X):
+		print("TRAIN:", len(train_index), "VALIDATION:", len(val_index))
+		x_train, x_val = [X[index] for index in train_index], [X[index] for index in val_index]
+		y_train, y_val = [str(Y[index]) for index in train_index], [str(Y[index]) for index in val_index]
 
-	# classify some data
-	out = wsd.classify(x_train)
+		# train using the input data
+		wsd.train(x_train,y_train)
 
-	# the output of classify is a string list in the same sequence as the input
-	for i,d in enumerate(x_train):
-	    print(out[i],d)
+		# classify train data
+		out_train = wsd.classify(x_train)
+
+		print(confusion_matrix(y_train, out_train))
+
+		return
+
 
 if __name__ == "__main__":
 	sys.exit(main())
